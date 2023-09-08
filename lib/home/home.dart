0f7b09c59026c227c16501a7ffa731d026bdbd56
Rafaz-dev <114362947+Rxfinha-dev@users.noreply.github.com>
2023-09-08 15:13:34 -0300

    colors

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -20,7 +20,7 @@ class HomeScreen extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-      primarySwatch: Colors.amber,
+        primarySwatch: Colors.amber,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Text(
                 "Controle de ovos",
-                style: TextStyle(fontSize: width),
+                style: TextStyle(fontSize: width, color: Colors.white),
               ),
             ],
           ),
